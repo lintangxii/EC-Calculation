b362601ce2781dfed70588990b7f43aba92ff926
lintangxii <lintangxii@gmail.com>
2026-07-20 19:50:21 +0700

    fix

--- scope.py
+++ scope.py
@@ -22,8 +22,9 @@ if not file_path:
     exit()
 
 # Read CSV
-save_file = 1  # Set to True if you want to save the output CSV
+save_file = 0  # Set to True if you want to save the output CSV
 data_clip = 0  # Set to True if you want to clip the data
+analytical_mode = 0  # Set to True if you want to run in analytical mode
 
 # Data clipping parameters
 data_skip = 1200
@@ -46,26 +47,27 @@ df["t_sec"] = np.arange(len(df)) / fs
 # Convert to datetime relative to first timestamp in the sliced data
 df["t_datetime"] = pd.to_datetime(df["t_sec"], unit="s", origin=df[time_col].iloc[0])
 
-# Peak detection for EC signal
-peaks_loadcell1, troughs_loadcell1, _ = detect_peaks(df["LoadCell1"], fs=fs/4, prominence_ratio=0.01, distance_sec=30)
+if analytical_mode == 1:
+    # Peak detection for EC signal
+    peaks_loadcell1, troughs_loadcell1, _ = detect_peaks(df["LoadCell1"], fs=fs/4, prominence_ratio=0.01, distance_sec=30)
 
-# critical_points_loadcell1 = np.sort(np.concatenate((peaks_loadcell1, troughs_loadcell1)))
-troughs = troughs_loadcell1
-peaks = peaks_loadcell1
+    # critical_points_loadcell1 = np.sort(np.concatenate((peaks_loadcell1, troughs_loadcell1)))
+    troughs = troughs_loadcell1
+    peaks = peaks_loadcell1
 
 
-# Find settling points for each trough
-settling_criteria = 0.982  # Settle to 98.2% of the peak value
-settling_points_indices = []
-for i in range(len(troughs) - 1):
-    settling_points_index, settling_target = find_settling_index(df["EC"], troughs[i], troughs[i+1], settle = settling_criteria, polyorder=2, window_length=21)
-    settling_points_indices.append(settling_points_index)
+    # Find settling points for each trough
+    settling_criteria = 0.982  # Settle to 98.2% of the peak value
+    settling_points_indices = []
+    for i in range(len(troughs) - 1):
+        settling_points_index, settling_target = find_settling_index(df["EC"], troughs[i], troughs[i+1], settle = settling_criteria, polyorder=2, window_length=21)
+        settling_points_indices.append(settling_points_index)
 
-# Find settling time
-settling_times = (np.array(settling_points_indices) - np.array(peaks))/fs
-print(f"Settling times (s): {settling_times.round(2)}")
-settling_weights = np.array(df["LoadCell1"].iloc[peaks]) - np.array(df["LoadCell1"].iloc[settling_points_indices])
-print(f"Settling weights (g): {settling_weights.round(2)}")
+    # Find settling time
+    settling_times = (np.array(settling_points_indices) - np.array(peaks))/fs
+    print(f"Settling times (s): {settling_times.round(2)}")
+    settling_weights = np.array(df["LoadCell1"].iloc[peaks]) - np.array(df["LoadCell1"].iloc[settling_points_indices])
+    print(f"Settling weights (g): {settling_weights.round(2)}")
 
 # Create figure with secondary axis
 fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -134,7 +136,9 @@ for i, col in enumerate(df.columns[1:]):
         secondary_y=(col.lower() in secondary_y)
     )
 
-fig.add_trace(
+
+if analytical_mode == 1:
+    fig.add_trace(
     go.Scatter(
         x=df["t_datetime"].iloc[troughs],
         y=df["LoadCell1"].iloc[troughs],
@@ -148,54 +152,53 @@ fig.add_trace(
     ),
     secondary_y=True
 )
-
-fig.add_trace(
-    go.Scatter(
-        x=df["t_datetime"].iloc[settling_points_indices],
-        y=df["LoadCell1"].iloc[settling_points_indices],
-        mode="markers",
-        name="LoadCell1 Settling Points",
-        marker=dict(
-            color="Green",
-            size=8,
-            symbol="x"
+    fig.add_trace(
+        go.Scatter(
+            x=df["t_datetime"].iloc[settling_points_indices],
+            y=df["LoadCell1"].iloc[settling_points_indices],
+            mode="markers",
+            name="LoadCell1 Settling Points",
+            marker=dict(
+                color="Green",
+                size=8,
+                symbol="x"
+            ),
         ),
-    ),
-    secondary_y=True
-)
+        secondary_y=True
+    )
 
-fig.add_trace(
-    go.Scatter(
-        x=df["t_datetime"].iloc[settling_points_indices],
-        y=df["EC"].iloc[settling_points_indices],
-        mode="markers",
-        name="EC Settling Points",
-        marker=dict(
-            color="Black",
-            size=8,
-            symbol="x"
+    fig.add_trace(
+        go.Scatter(
+            x=df["t_datetime"].iloc[settling_points_indices],
+            y=df["EC"].iloc[settling_points_indices],
+            mode="markers",
+            name="EC Settling Points",
+            marker=dict(
+                color="Black",
+                size=8,
+                symbol="x"
+            ),
         ),
-    ),
-    secondary_y=False
-)
+        secondary_y=False
+    )
 
-fig.add_trace(
-    go.Scatter(
-        x=df["t_datetime"].iloc[peaks],
-        y=df["LoadCell1"].iloc[peaks],
-        mode="markers",
-        name="Concentration starting point",
-        marker=dict(
-            color="Red",
-            size=8,
-            symbol="x"
+    fig.add_trace(
+        go.Scatter(
+            x=df["t_datetime"].iloc[peaks],
+            y=df["LoadCell1"].iloc[peaks],
+            mode="markers",
+            name="Concentration starting point",
+            marker=dict(
+                color="Red",
+                size=8,
+                symbol="x"
+            ),
         ),
-    ),
-    secondary_y=True
-)
+        secondary_y=True
+    )
 
 fig.update_layout(
-    title=os.path.basename(file_path) + f"\nSettling criteria: {settling_criteria*100:.1f}%",
+    title=os.path.basename(file_path) + f"\nSettling criteria: {settling_criteria*100:.1f}%" if analytical_mode == 1 else os.path.basename(file_path),
     template="plotly_white",
     hovermode="x unified",
     legend=dict(
